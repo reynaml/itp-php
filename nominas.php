3122ccd2abbca58fb80abf10ef86dcc0dcf05e63
reyna <reynaml_42@hotmail.com>
2014-06-11 11:17:32 -0500

    modificaciones de nominas

--- nominas.php
+++ nominas.php
@@ -1,12 +1,12 @@
 <?php
-$empleadoss = require 'empleados.php';
+$empleadoss = require 'empleadoss.php';
 
 nominas_foreach($empleadoss);
 
 function nominas_foreach($empleadoss)
 {
 echo "Nombre                 | Sueldo\n";
-foreach ($empleados as $empleado){
+foreach ($empleadoss as $empleado){
 	$sueldo = $empleado['sueldo'] - $empleado['sueldo'] * .15 - $empleado['sueldo'] *.10;
 	
 	echo "{$empleado['nombre']}             | $sueldo\n";
