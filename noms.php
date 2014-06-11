2d4484f84b65dd2590b0c16f4b84e0cb65a92812
reyna <reynaml_42@hotmail.com>
2014-06-11 14:02:59 -0500

    ejercicio de empleados nomina con deptos

--- noms.php
+++ noms.php
@@ -1,15 +1,27 @@
 <?php
-$empleados = require 'empleados.php';
+$empleados = require 'emp.php';
 
-echo "Nombre                 | Sueldo\n";
+echo "Nombre                 | Sueldo                 |  Departamento \n";
 
 foreach ($empleados as $empleado){
 	$sueldo = $empleado['sueldo'] 
 	- $empleado['sueldo'] * .15 
 	- $empleado['sueldo'] *.10;
 	
-	echo "{$empleado['nombre']}             | $sueldo\n";
+	echo "{$empleado['nombre']}             | $sueldo            |'depar'\n";
 
 }
+
+$depa = function($dep){
+		if($dep == 'tintes'){
+		return $dep;
+		} else {
+		return 'armado';
+		}
+};
+echo "{$empleado['nombre']}             | $sueldo            |'depar'\n";
+echo "\n";
+
+
 
 ?>
