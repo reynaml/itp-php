8b2d2230ef67879097de19765f51660b7deea8cf
reyna <reynaml_42@hotmail.com>
2014-06-11 14:02:27 -0500

    ejercicio de empleados con dep

--- emp.php
+++ emp.php
@@ -4,26 +4,73 @@ return [
 	[
 	'nombre' => 'Alejandro',
 	'sueldo' => '250',
+	'depar' => 'tintes'
 	],
 	
 	[
-	'nombre' => 'Selene',
+	'nombre' => 'Maria',
+	'sueldo' => '100',
+	'depar' => 'tintes'
+	],
+	
+	[
+	'nombre' => 'Armando',
 	'sueldo' => '300',
+	'depar' => 'tintes'
 	],
 	
 	[
-	'nombre' => 'Alma',
-	'sueldo' => '600',
+	'nombre' => 'Carlos',
+	'sueldo' => '150',
+	'depar' => 'tintes'
+	],
+	
+	[
+	'nombre' => 'Adrian',
+	'sueldo' => '350',
+	'depar' => 'tintes'
+	],
+	
+		[
+	'nombre' => 'Manuel',
+	'sueldo' => '400',
+	'depar' => 'tintes',
 	],
 
 	[
-	'nombre' => 'Maru',
-	'sueldo' => '500',
+	'nombre' => 'Dolores',
+	'sueldo' => '450',
+	'depar'  => 'armado',
 	],
 	
 	[
-	'nombre' => 'Mariana',
-	'sueldo' => '900',
+	'nombre' => 'Rosa',
+	'sueldo' => '500',
+	'depar'  => 'armado',
+	],
+	
+	[
+	'nombre' => 'Juan',
+	'sueldo' => '550',
+	'depar'  => 'armado',
+	],
+	
+	[
+	'nombre' => 'Jose',
+	'sueldo' => '600',
+	'depar'  => 'armado',
+	],
+	
+	[
+	'nombre' => 'Veronica',
+	'sueldo' => '650',
+	'depar'  => 'armado',
+	],
+	
+	[
+	'nombre' => 'Camila',
+	'sueldo' => '700',
+	'depar'  => 'armado',
 	],
 
 ]
