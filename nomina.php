df68b98f696327aabf6131000a2513710d7491af
reyna <reynaml_42@hotmail.com>
2014-06-11 11:05:43 -0500

    correccion de espacios en nomina

--- nomina.php
+++ nomina.php
@@ -1,12 +1,12 @@
 <?php
 $empleados = require 'empleados.php';
 
-echo "Nombre      | Sueldo\n";
+echo "Nombre                 | Sueldo\n";
 
 foreach ($empleados as $empleado){
 	$sueldo = $empleado['sueldo'] - $empleado['sueldo'] * .15 - $empleado['sueldo'] *.10;
 	
-	echo "{$empleado['nombre']}  | $sueldo\n";
+	echo "{$empleado['nombre']}             | $sueldo\n";
 
 }
 
