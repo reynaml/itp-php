f962089a8cf79763afba826821d0d599b4ad60e2
reyna <reynaml_42@hotmail.com>
2014-06-11 13:03:57 -0500

    complemento del ejercicio de suma

--- suma.php
+++ suma.php
@@ -4,10 +4,13 @@ function suma()
 
 	$suma = 0;
 	foreach (func_get_args() as $number){
+	if(!is_numeric($number)) continue;
 	$suma += $number;
 	}
 	
 	return $suma;
 }
 echo suma(2, 5, 6, 7, 8, 10);
+echo suma();
+echo suma('yuyuyuy', 98, true);
 ?>
