6fe8dc47f00630bcf0747a9d449e5be8cff9666f
reyna <reynaml_42@hotmail.com>
2014-06-11 11:18:20 -0500

    ejemplo de nomina con ciclo for

---
+++ empleados1.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+	[
+	'nombre' => 'Alejandro',
+	'sueldo' => '250',
+	],
+	
+	[
+	'nombre' => 'Selene',
+	'sueldo' => '300',
+	],
+	
+	[
+	'nombre' => 'Alma',
+	'sueldo' => '600',
+	],
+
+	[
+	'nombre' => 'Maru',
+	'sueldo' => '500',
+	],
+	
+	[
+	'nombre' => 'Mariana',
+	'sueldo' => '900',
+	],
+
+]
+
+?>
