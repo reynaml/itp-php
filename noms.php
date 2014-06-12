7d3d7813005b57413d293ab2d4a0346787079d4b
reyna <reynaml_42@hotmail.com>
2014-06-12 10:57:01 -0500

    ejercicio con funcion anonima

--- noms.php
+++ noms.php
@@ -12,15 +12,14 @@ echo "Nombre    |  Depar | Sue base | IVA | ISR | Sue total | Impuesto total\n";
 foreach ($departamentos as $departamento => $empleados){
 	echo "\n $departamento ";
 	$impuesto= 0;
-		foreach ($empleados as $empleado){
+		array_walk($empleados, function($empleado) use ($departamento, &$impuesto){
 	$iva=$empleado['sueldo'] * .15 ;
 	$isr=$empleado['sueldo']* .10 ;
 	$sueldo = $empleado['sueldo'] - $iva - $isr;
 	$impuesto +=$sueldo;
-	
 	echo "{$empleado['nombre']}   |{$empleado['sueldo']} |\n";
 	echo "$iva    | $isr    | $sueldo |\n";
-}
+});
 
 echo "\nImpuesto Total:  $impuesto \n";
 }
