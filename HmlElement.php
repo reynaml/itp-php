4107f0d9a0672e5f20d8651110c8ecc5e330f92a
reyna <reynaml_42@hotmail.com>
2014-06-12 13:21:13 -0500

    ejemplos de metodos magicos

---
+++ HmlElement.php
@@ -0,0 +1,77 @@
+<?php
+class HtmlElement
+{
+	protected $tag;
+	protected $content;
+	protected $attributes;
+	
+	public function __construct(
+	$tag,
+	$attributes=[],
+	$content = null
+	)
+	{
+	$this->tag = $tag;
+	$this->content = $content;
+	$this->attributes = $attributes;
+	}
+	public  function __call($name, $arguments)
+	{
+	/*if('render' === $name){
+		$html=sprintf(
+		'<%s  %s> %s </%s>',
+		$this ->tag,
+		implode ('=', $this->attributes),
+		$this ->content,
+		$this ->tag
+		);
+		return $html;
+	}*/
+	//var_dump($name);
+	//var_dump($arguments);
+	}
+	
+	public function render()
+	{
+	$html=sprintf(
+		'<%s  %s> %s </%s>',
+		$this ->tag,
+		implode ('=', $this->attributes),
+		$this ->content,
+		$this ->tag
+		);
+		return $html;
+	}
+	
+	public function __toString()
+	{
+	return $this-> render();
+	}
+	
+	public function __get($property)
+	{
+	if ('tag'  === $property){
+		return $this->tag;
+		}
+	}
+	
+	public function __set($property, $value)
+	{
+	if ('content' === $property){
+		$this->content =$value;
+		}
+	}
+}
+$link = new HtmlElement('a', ['href=http://www.google.com.mx'], 'Google');
+echo $link ->render(['class' => 'important-link']);
+echo "\n";
+echo $link->tag;
+echo "\n";
+$link->content ='yahoo';
+echo "\n";
+echo $link;
+
+//<a href="http://www.google.com.mx"> Google </a>
+//<span> nombre </span>
+//<br>
+?>
