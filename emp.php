fb38f18af596f995131965f4899c273a67905bc0
reyna <reynaml_42@hotmail.com>
2014-06-12 10:10:27 -0500

    ejercicio de departamentos

--- emp.php
+++ emp.php
@@ -1,77 +1,71 @@
 <?php
 
 return [
+
+	'Tintas' =>[
+	
 	[
 	'nombre' => 'Alejandro',
 	'sueldo' => '250',
-	'depar' => 'tintes'
 	],
 	
 	[
 	'nombre' => 'Maria',
 	'sueldo' => '100',
-	'depar' => 'tintes'
 	],
 	
 	[
 	'nombre' => 'Armando',
 	'sueldo' => '300',
-	'depar' => 'tintes'
 	],
 	
 	[
 	'nombre' => 'Carlos',
 	'sueldo' => '150',
-	'depar' => 'tintes'
 	],
 	
 	[
 	'nombre' => 'Adrian',
 	'sueldo' => '350',
-	'depar' => 'tintes'
 	],
 	
-		[
+	[
 	'nombre' => 'Manuel',
 	'sueldo' => '400',
-	'depar' => 'tintes',
 	],
-
+],
+	
+'Armados' =>[
 	[
 	'nombre' => 'Dolores',
 	'sueldo' => '450',
-	'depar'  => 'armado',
 	],
 	
 	[
 	'nombre' => 'Rosa',
 	'sueldo' => '500',
-	'depar'  => 'armado',
 	],
 	
 	[
 	'nombre' => 'Juan',
 	'sueldo' => '550',
-	'depar'  => 'armado',
 	],
 	
 	[
 	'nombre' => 'Jose',
 	'sueldo' => '600',
-	'depar'  => 'armado',
 	],
 	
 	[
 	'nombre' => 'Veronica',
 	'sueldo' => '650',
-	'depar'  => 'armado',
 	],
 	
 	[
 	'nombre' => 'Camila',
 	'sueldo' => '700',
-	'depar'  => 'armado',
 	],
+]
 
 ]
 
